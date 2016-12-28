4b374e3a55c7786d7f4ff63a8f356d5cabcf69ce
Eralp Erat <eralp@pragmalinq.com>
2016-12-29 02:35:25 +0300

    travis CI support

---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestSaveJSON(t *testing.T) {
+	t.Skip("Save JSON is skipping.!")
+}
+
+func TestCurrencyDay_GetData(t *testing.T) {
+	t.Skip("CurrencyDay is skipping.!")
+}
